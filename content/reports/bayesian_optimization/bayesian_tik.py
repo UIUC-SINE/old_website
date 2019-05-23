638f695ce93725f6dff1a97570352a34e34fb24f
evan <evan@evanw.org>
2019-05-22 20:05:23 -0500

    update bayesian_tik for multiple sources, add lambda plots

--- content/reports/bayesian_optimization/bayesian_tik.py
+++ content/reports/bayesian_optimization/bayesian_tik.py
@@ -1,45 +1,49 @@
 from mas.psf_generator import PhotonSieve, PSFs
 from mas.forward_model import add_noise, get_measurements
 from mas.data import strands
+from mas import sse_cost
+from mas.csbs import csbs
 from mas.measure import compare_ssim
 from bayes_opt import BayesianOptimization
 from mas.deconvolution import tikhonov
 import numpy as np
 from matplotlib import pyplot as plt
 
-truth = strands[0:1]
+truth = strands[0:2]
 ps = PhotonSieve()
 
-wavelengths = np.array([33.4e-9])
+wavelengths = np.array([33.4e-9, 33.5e-9])
 
 psfs = PSFs(
     ps,
     source_wavelengths=wavelengths,
-    measurement_wavelengths=wavelengths,
-    num_copies=1
+    # measurement_wavelengths=wavelengths,
+    # num_copies=6,
+    measurement_wavelengths=10,
+    num_copies=12
 )
-measured = get_measurements(psfs=psfs, sources=truth, real=True)
-measured = add_noise(measured, model='poisson', max_count=10)
+
 
 # Bounded region of parameter space
-pbounds = {'tikhonov_lam': (5e-20, 5e-1)}
+pbounds = {'tikhonov_lam_exp': (-8, 1)}
 
-def cost(tikhonov_lam):
+def cost(tikhonov_lam_exp):
 
+    psfs.copies = np.repeat(12, 12)
+    csbs(psfs, sse_cost, 12, lam=10**tikhonov_lam_exp, order=1)
+    measured = get_measurements(psfs=psfs, sources=truth, real=True)
+    measured_noisy = add_noise(measured, model='poisson', max_count=500)
     recon = tikhonov(
             sources=measured,
             psfs=psfs,
-            measurements=measured,
-            tikhonov_lam=tikhonov_lam
-    )[0]
+            measurements=measured_noisy,
+            tikhonov_lam=10**tikhonov_lam_exp
+    )
 
-    plt.imshow(recon)
+    plt.imshow(recon[0])
     plt.show()
     plt.pause(.05)
-    return compare_ssim(
-        truth[0],
-        recon
-    )
+    return compare_ssim(truth[0], recon[0]) + compare_ssim(truth[1], recon[1])
 
 optimizer = BayesianOptimization(
     cost,
@@ -51,6 +55,16 @@ optimizer = BayesianOptimization(
 
 np.seterr(all='ignore')
 optimizer.maximize(
+    acq='ei',
+    xi=1e-4,
     init_points=10,
     n_iter=30,
 )
+
+searched = np.array([
+    [
+            x['params']['tikhonov_lam_exp'],
+            x['target']
+    ] for x in optimizer.res
+])
+plt.plot(searched[:, 0], searched[:, 1])
