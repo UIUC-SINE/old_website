1b2ae75c61c13f5f7e8ca553841c990b861edfe3
ukamaci <kamaci.ulas@gmail.com>
2019-12-01 21:02:27 -0600

    update the visors simulation code
    -update the registration: it takes argmax after summing the 
    correlations, then takes the mean of groups
    
    -add image deblurring using the estimated motion blur kernel
    
    -use 750x750 images rather than 160x160

--- content/reports/2019-11-11_drift/forward_model_drift.py
+++ content/reports/2019-11-11_drift/forward_model_drift.py
@@ -4,21 +4,29 @@
 
 import numpy as np
 import copy
-from skimage.transform import rescale
+from skimage.transform import rescale, resize
 from scipy.stats import poisson
-from skimage.draw import line
+from skimage.draw import line, line_aa
 from mas.strand_generator import strands
 from mas.psf_generator import PSFs, PhotonSieve, circ_incoherent_psf
 from mas.forward_model import get_measurements, add_noise, size_equalizer, rectangle_adder
 from matplotlib import pyplot as plt
 from matplotlib.patches import Rectangle
 from skimage.feature import register_translation
+from scipy.ndimage import fourier_shift
+from scipy.ndimage.filters import gaussian_filter
+from scipy.signal import convolve2d
+from mas.deconvolution import tikhonov, admm
+from mas.deconvolution.admm import bm3d_pnp
+from functools import partial
+
 
 # %% scene
-resolution_ratio = 5 # resolution_ratio = ccd_pixel_size / simulated_scene_pixel_size
+ccd_size = [750,750] # number of (rows,columns) in the photodetector array
+resolution_ratio = 2 # resolution_ratio = ccd_pixel_size / simulated_scene_pixel_size
 fov_ratio = 2 # fov_ratio = simulated_imge_fov / ccd_fov
-scene = strands(num_strands=100*fov_ratio, thickness=22*resolution_ratio, min_angle=-20, max_angle=20,
-            image_width=160*resolution_ratio*fov_ratio, initial_width=512*resolution_ratio*fov_ratio)
+scene = strands(num_strands=int(100*fov_ratio*ccd_size[0]/160), thickness=22*resolution_ratio, min_angle=-20, max_angle=20,
+            image_width=ccd_size[0]*resolution_ratio*fov_ratio, initial_width=int(ccd_size[0]*512/160)*resolution_ratio*fov_ratio)
 
 plt.imshow(scene, cmap='gist_heat')
 plt.show()
@@ -26,12 +34,12 @@ plt.show()
 # %% params
 # exposure parameters
 total_exp_time = 10 # in seconds
-frame_rate = 4 # in Hz
+frame_rate = 8 # in Hz
 num_frames = int(total_exp_time*frame_rate)
 
 # drift parameters
 drift_angle = 10 * (2*np.pi/360) # in radians
-drift_velocity = 0.2e-3 # in meter/second
+drift_velocity = 0.3e-3 # in meter/second
 # drift_velocity = 0 # in meter/second
 
 # signal & noise parameters
@@ -42,12 +50,12 @@ background_noise = 2 # in count/second/pixel
 nonoise = False # test parameter to simulate noise-free frames
 
 # ccd parameters
-ccd_size = [160,160] # number of (rows,columns) in the photodetector array
 pixel_size = 14e-6 # in meters
 
 # sieve parameters
 diameter = 75e-3 # in meters
 smallest_hole_diameter = 17e-6 # in meters
+wavelengths = np.array([30.4e-9])
 
 # %% forward
 ps = PhotonSieve(diameter=diameter, smallest_hole_diameter=smallest_hole_diameter)
@@ -56,8 +64,8 @@ ps = PhotonSieve(diameter=diameter, smallest_hole_diameter=smallest_hole_diamete
 psfs = PSFs(
     ps,
     sampling_interval=pixel_size/resolution_ratio,
-    measurement_wavelengths=np.array([30.4e-9]),
-    source_wavelengths=np.array([30.4e-9]),
+    measurement_wavelengths=wavelengths,
+    source_wavelengths=wavelengths,
     psf_generator=circ_incoherent_psf,
     cropped_width=101,
     num_copies=1
@@ -76,7 +84,7 @@ topleft_coords = [ # calculate the topleft points for all frames
     for k in range(num_frames+1)
 ]
 
-frames = np.zeros((num_frames, ccd_size[0], ccd_size[1])) # initialize the frame images
+frames_clean = np.zeros((num_frames, ccd_size[0], ccd_size[1])) # initialize the frame images
 
 # calculate each frame by integrating high resolution image along the drift
 # direction
@@ -94,15 +102,17 @@ for frame in range(num_frames):
         path_rows, path_cols = path_rows[:-1], path_cols[:-1]
     for row,col in zip(path_rows, path_cols):
         temp += convolved[row:row+temp.shape[0], col:col+temp.shape[1]]
-    frames[frame] = rescale(temp, 1/resolution_ratio, anti_aliasing=False)
+    frames_clean[frame] = rescale(temp, 1/resolution_ratio, anti_aliasing=False)
 
 # add noise to the frames
 if nonoise is False:
     frames = poisson.rvs(
-        frames / frames.max() * (max_count_rate / frame_rate) +
+        frames_clean / frames_clean.max() * (max_count_rate / frame_rate) +
         (dark_current + background_noise) / frame_rate
     )
     frames = np.random.normal(loc=frames, scale=read_noise)
+    # set the negative values to zero
+    frames[frames < 0] = 0
 
 # %% plot
 pixel_arcsec = 72e-3
@@ -139,37 +149,199 @@ for i in range(len(frames)):
         plt.cla()
 
 
-# %% registration --------------------------------------------------------------
-true_pixel_drift = drift_velocity / frame_rate * np.array([
+# %% register --------------------------------------------------------------
+true_shift = drift_velocity / frame_rate * np.array([
     np.cos(drift_angle),
     np.sin(drift_angle)
 ]) / pixel_size # true drift from one to the next frame. notation: (x,y)
 
-# form the array that will hold all the estimated pairwise shifts between the frames
-est_pixel_drift = np.zeros((int(num_frames * (num_frames-1) / 2), 2)) # notation: (rows,cols) (-y,x)
-counter = 0
+print('true_shift:({},{})'.format(true_shift[0],true_shift[1]))
+
+# initialize the array that holds fourier transforms of the correlations between frame pairs
+correlations_f = np.zeros((num_frames-1, frames.shape[1], frames.shape[2])).astype(np.complex128)
+
 for i in range(num_frames-1):
     for j in np.arange(i+1, num_frames):
-        est_pixel_drift[counter], _, _ = register_translation(frames[i], frames[j])
-        est_pixel_drift[counter] = est_pixel_drift[counter] / (j-i)
-        counter += 1
+        # k^th element of correlations_f is the sum of all correlations between
+        # frame pairs that are k frame apart from each other.
+        correlations_f[j-i-1] += np.fft.fftn(frames[i]) * np.fft.fftn(frames[j]).conj()
 
-est_pixel_drift_refined = copy.deepcopy(est_pixel_drift)
-plt.figure()
-plt.scatter(est_pixel_drift_refined[:,1], -est_pixel_drift_refined[:,0], s=3) # notation: plt.scatter(x,y)
-for i in range(10):
-    est_pixel_drift_refined = est_pixel_drift_refined[np.linalg.norm(
-        est_pixel_drift_refined - np.mean(est_pixel_drift_refined, axis=0),
-        axis=1
-    ) < 40 * 2**(-i)
-    ]
-    plt.scatter(est_pixel_drift_refined[:,1], -est_pixel_drift_refined[:,0], s=3)
-    print(np.mean(est_pixel_drift_refined, axis=0))
+correlations = np.fft.ifft2(correlations_f).real
+
+for i in range(len(correlations)):
+    # convolve the correlations with a gaussian to eliminate outlier peaks
+    correlations[i] = gaussian_filter(correlations[i], sigma=1, mode='wrap')
+
+shifts = np.zeros((correlations.shape[0], 2))
+
+for i in range(correlations.shape[0]):
+    # find the argmax points, which give the estimated shift
+    shifts[i] = np.unravel_index(np.argmax(correlations[i]), correlations[i].shape)
+
+# bring the shift values from [0,N] to [-N/2, N/2] format
+shifts[shifts>np.fix(frames[0].shape[0]/2)] -= frames[0].shape[0]
+
+# normalize the shifts to per frame shift
+shifts = shifts / np.tile(np.arange(1,shifts.shape[0]+1), (2,1)).T
+
+# determine what proportion of the shift estimates to use in the final shift estimation
+# if `proportion` < 1, then we are not using the correlations of frame pairs that
+# are very far from each other, the reason being that further apart frames have
+# less overlap, where the nonoverlapping parts contribute to the correlation as
+# 'noise', and reduce the accuracy.
+proportion = 0.4
+
+# estimate the shift using the first `proportion` of the shift array
+shift_est = np.mean(shifts[:int(proportion * shifts.shape[0])], axis=0)
+print('shift_est:({},{})'.format(shift_est[1],-shift_est[0]))
+
+
+# initialize the array that will take the fourier transform of the correlations of correlations
+correlations_f2 = np.zeros((num_frames-2, frames.shape[1], frames.shape[2])).astype(np.complex128)
+
+for i in range(num_frames-2):
+    for j in np.arange(i+1, num_frames-1):
+        # compute the correlations between correlations to get a more refined estimate of drift
+        correlations_f2[j-i-1] += np.fft.fftn(correlations[i]) * np.fft.fftn(correlations[j]).conj()
+
+correlations2 = np.fft.ifft2(correlations_f2).real
+
+for i in range(len(correlations2)):
+    # convolve the correlations with a gaussian to eliminate outlier peaks
+    correlations2[i] = gaussian_filter(correlations2[i], sigma=1, mode='wrap')
+
+shifts2 = np.zeros((correlations2.shape[0], 2))
+
+for i in range(correlations2.shape[0]):
+    shifts2[i] = np.unravel_index(np.argmax(correlations2[i]), correlations2[i].shape)
+
+shifts2[shifts2>np.fix(frames[0].shape[0]/2)] -= frames[0].shape[0]
+shifts2 = shifts2 / np.tile(np.arange(1,shifts2.shape[0]+1), (2,1)).T
+
+shift_est2 = -np.mean(shifts2[:int(proportion * shifts2.shape[0])], axis=0)
+print('shift_est2:({},{})'.format(shift_est2[1],-shift_est2[0]))
 
 registered = np.zeros_like(frames[0])
 for i in range(num_frames):
-    registered += np.fft.ifft2(fourier_shift(np.fft.fft2(frames[i]), i*np.mean(est_pixel_drift_refined, axis=0))).real
+    registered += np.fft.ifft2(fourier_shift(np.fft.fft2(frames[i]), i*shift_est2)).real
 
 plt.figure()
 plt.imshow(registered, cmap='gist_heat')
 plt.title('Registered Image')
+plt.show()
+
+
+# %% deblur --------------------------------------------------------------
+# first create a motion blur kernel on a grid with 1 um pixel size
+pixel_size_um = pixel_size * 1e6
+
+# width of the final motion blur kernel with CCD pixel size
+kernel_size = 11
+(x,y) = (pixel_size_um * shift_est2[1], -pixel_size_um * shift_est2[0])
+N = int(np.ceil(np.max((abs(x),abs(y)))))
+
+# set the shape of the initial kernel with 1 um pixels based on the estimated drift
+kernel_um = np.zeros((2*N+1, 2*N+1))
+
+# calculate the line representing the motion blur
+rr, cc, val = line_aa(
+    N + np.round((y/2)).astype(int),
+    N - np.round((x/2)).astype(int),
+    N - np.round((y/2)).astype(int),
+    N + np.round((x/2)).astype(int),
+)
+
+# update the kernel with the calculated line
+kernel_um[rr,cc] = val
+
+# resize the initial 1 um kernel to the given pixel size
+kernel = resize(size_equalizer(kernel_um, [int(pixel_size_um)*kernel_size]*2), [kernel_size]*2, anti_aliasing=True)
+
+# compute the analytical photon sieve PSF with the given pixel size
+psfs = PSFs(
+    ps,
+    sampling_interval=pixel_size,
+    measurement_wavelengths=wavelengths,
+    source_wavelengths=wavelengths,
+    psf_generator=circ_incoherent_psf,
+    cropped_width=kernel_size,
+    num_copies=1
+)
+
+psf_orig = copy.deepcopy(psfs.psfs[0,0])
+
+# convolve the photon sieve PSF with the motion blur kernel to find the "effective blurring kernel"
+psfs.psfs[0,0] = convolve2d(psfs.psfs[0,0], kernel, mode='same')
+
+# normalize the kernel
+psfs.psfs[0,0] /= psfs.psfs[0,0].sum()
+
+# normalize the registered image (doesn't change anything but helps choosing regularization parameter consistently)
+registered /= registered.max()
+
+# do a tikhonov regularized deblurring on the registered image to remove the
+# in-frame blur with the calculated "effective blurring kernel"
+recon_tik = tikhonov(
+    measurements=registered[np.newaxis,:,:],
+    psfs=psfs,
+    tikhonov_lam=1e1,
+    tikhonov_order=1
+)
+plt.figure()
+plt.imshow(recon_tik[0], cmap='gist_heat')
+plt.title('Deblurred Tikhonov')
+plt.show()
+
+# do a Plug and Play with BM3D reconstruction with tikhonov initialization
+recon = admm(
+    measurements=registered[np.newaxis,:,:],
+    psfs=psfs,
+    regularizer=partial(bm3d_pnp),
+    recon_init=recon_tik,
+    iternum=5,
+    periter=1,
+    nu=10**0.0,
+    lam=[10**-0.5]
+)
+
+plt.figure()
+plt.imshow(recon[0], cmap='gist_heat')
+plt.title('Deblurred')
+plt.show()
+
+
+# This commented out part is my (Ulas') first registration implementation that
+# is based on taking the mean of the argmaxes.
+# ----------------------------------------------------------------------------
+# # form the array that will hold all the estimated pairwise shifts between the frames
+# est_pixel_drift = np.zeros((int(num_frames * (num_frames-1) / 2), 2)) # notation: (rows,cols) (-y,x)
+# counter = 0
+# for i in range(num_frames-1):
+#     for j in np.arange(i+1, num_frames):
+#         est_pixel_drift[counter], _, _ = register_translation(frames[i], frames[j], upsample_factor=20)
+#         est_pixel_drift[counter] = est_pixel_drift[counter] / (j-i)
+#         counter += 1
+#
+# est_pixel_drift_refined = copy.deepcopy(est_pixel_drift)
+# plt.figure()
+# plt.scatter(est_pixel_drift_refined[:,1], -est_pixel_drift_refined[:,0], s=3) # notation: plt.scatter(x,y)
+# for i in range(10):
+#     shrinked_list = est_pixel_drift_refined[np.linalg.norm(
+#         est_pixel_drift_refined - np.mean(est_pixel_drift_refined, axis=0),
+#         axis=1
+#     ) < 40 * 2**(-i)
+#     ]
+#     if shrinked_list.shape[0] > 0:
+#         est_pixel_drift_refined = shrinked_list
+#     else:
+#         break
+#     plt.scatter(est_pixel_drift_refined[:,1], -est_pixel_drift_refined[:,0], s=3)
+#     print(np.mean(est_pixel_drift_refined, axis=0))
+#
+# registered = np.zeros_like(frames[0])
+# for i in range(num_frames):
+#     registered += np.fft.ifft2(fourier_shift(np.fft.fft2(frames[i]), i*np.mean(est_pixel_drift_refined, axis=0))).real
+#
+# plt.figure()
+# plt.imshow(registered, cmap='gist_heat')
+# plt.title('Registered Image')
