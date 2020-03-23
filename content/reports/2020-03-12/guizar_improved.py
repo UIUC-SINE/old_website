6d769cda6727de3b3bd0e57d37c5435ece3cc694
evan <evan@evanw.org>
2020-03-23 07:44:10 -0500

    clean up guizar comparison

--- content/reports/2020-03-12/guizar_improved.py
+++ content/reports/2020-03-12/guizar_improved.py
@@ -1,4 +1,4 @@
-from mas.tracking import guizar_multiframe, correlate_and_sum
+from mas.tracking import guizar_multiframe, correlate_and_sum, guizar_upsample
 from mas.strand_generator import StrandVideo
 from mas.misc import combination_experiment
 import numpy as np
@@ -13,22 +13,10 @@ def experiment(*, max_count, drift_velocity, drift_angle, **kwargs):
 
     corr_sum = correlate_and_sum(sv.frames)
 
-    _, d = guizar_multiframe(corr_sum, start=1, end=N)
+    drift_new1, d = guizar_multiframe(corr_sum, start=1, end=N)
     drift_old = np.mean(d[9:29], axis=0)
 
-    n = np.arange(1, N)
-    weights1 = n
-    weights2 = n * (N - n)**2 / (n + (N - n)**2)
-
-    # FIXME - why is [-1] so bad sometimes? single frame correlation?
-    #         shouldnt [-2] be half as bad?
-    weights1[-2:] = 0
-
-    weights1 = weights1 / np.sum(weights1)
-    weights2 = weights2 / np.sum(weights2)
-
-    drift_new1 = np.sum(d * weights1[:, np.newaxis], axis=0)
-    drift_new2 = np.sum(d * weights2[:, np.newaxis], axis=0)
+    drift_new2 = guizar_upsample(corr_sum)[0]
 
     # import ipdb
     # ipdb.set_trace()
@@ -39,12 +27,10 @@ def experiment(*, max_count, drift_velocity, drift_angle, **kwargs):
         'error_new2': np.linalg.norm(sv.true_drift - drift_new2),
         'drift_old': drift_old,
         'drift_new1': drift_new1,
-        'drift_new2': drift_new1,
+        'drift_new2': drift_new2,
         'errorv_old': sv.true_drift - drift_old,
         'errorv_new1': sv.true_drift - drift_new1,
         'errorv_new2': sv.true_drift - drift_new2,
-        'weights1': weights1,
-        'weights2': weights1,
         'd': d,
         'd_err': np.linalg.norm(sv.true_drift - d, axis=1),
         'true_drift': sv.true_drift
@@ -54,11 +40,11 @@ def experiment(*, max_count, drift_velocity, drift_angle, **kwargs):
 
 results = combination_experiment(
     experiment,
-    max_count=[20, 40, 80],
-    drift_velocity=[0.01e-3, 0.05e-3, 0.2e-3],
-    drift_angle=np.linspace(0, -90, 4),
-    # max_count=[20],
-    # drift_velocity=[0.2e-3],
-    # drift_angle=[-45],
-    repetition=np.arange(30)
+    # max_count=[20, 40, 80],
+    # drift_velocity=[0.01e-3, 0.05e-3, 0.2e-3],
+    # drift_angle=np.linspace(0, -90, 4),
+    max_count=[20],
+    drift_velocity=[0.2e-3],
+    drift_angle=[-45],
+    repetition=np.arange(50)
 )
